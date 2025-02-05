e7909364ed9aa62275c2b5ddb14411b964db126c
Robert Ostrowski-Jagoda <robert.ostrowski.jagoda@gmail.com>
2025-02-05 19:43:20 +0100

    Match tests pass

--- src/main/java/org/robert/ostrowski/jagoda/Match.java
+++ src/main/java/org/robert/ostrowski/jagoda/Match.java
@@ -10,6 +10,16 @@ public class Match {
     protected volatile int awayTeamScore;
 
     Match(String homeTeamName, String awayTeamName) {
+        if (homeTeamName == null || homeTeamName.isEmpty())
+            throw new IllegalArgumentException("Home team name can't be null or empty");
+        if (awayTeamName == null || awayTeamName.isEmpty())
+            throw new IllegalArgumentException("Away team name can't be null or empty");
+        if (homeTeamName.equals(awayTeamName))
+            throw new IllegalArgumentException("Home team name can't be the same as away team name");
+        if (homeTeamName.contains(":"))
+            throw new IllegalArgumentException("Home team name can't contain ':' special character");
+        if (awayTeamName.contains(":"))
+            throw new IllegalArgumentException("Away team name can't contain ':' special character");
         this.homeTeamName = homeTeamName;
         this.awayTeamName = awayTeamName;
     }
